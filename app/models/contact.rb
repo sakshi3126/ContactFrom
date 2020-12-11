cb9f6418de777711f0beb7726550a87efa0644d2
sakshi-yadav <sakshi.yadav@bulkmro.com>
2020-12-11 18:28:01 +0530

    language translator added for validation error message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,11 @@
 class Contact < ApplicationRecord
-  validates :first_name, presence:true
-  validates_presence_of :last_name
-  validates :email, presence: true
+  validates :first_name, presence: { message: I18n.translate('warning_message.field_is_required') }
+  validates :last_name, presence: { message: I18n.translate('warning_message.field_is_required') }
+  validates :email, presence: { message: I18n.translate('warning_message.field_is_required') }
   # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :phone_number, format: { with: /\d[0-9]\)*\z/,
                                     message: I18n.translate('warning_message.phone_error') }
-  validates_presence_of :message
+  validates :message, presence: { message: I18n.translate('warning_message.field_is_required') }
 
   def full_name
     [first_name, last_name].compact.join(' ').titleize if first_name.present?
